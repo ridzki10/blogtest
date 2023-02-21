1b268aa42c551ad6d47a33e113dc1cffba9b0340
ridzki10 <ridzki.okta96@gmail.com>
2023-02-21 07:08:38 +0700

    add title text

--- components/Banner.js
+++ components/Banner.js
@@ -19,11 +19,18 @@ function Slide() {
                     <Image src={"/images/img1.jpg"} width={600} height={600}/>
                 </Link>
             </div>
-            <div className="info">
+            <div className="info flex justify-center flex-col">
                 <div className="cat">
                     <Link href={"/"} className="text-orange-600 hover:text-orange-800">Bussiness Travel </Link>
                     <Link href={"/"} className="text-gray-800 hover:text-gray-600">- Feb 21, 2023</Link>
                 </div>
+                <div className="title">
+                    <Link href={"/"} className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning </Link>
+                </div>
+                <p className='text-gray-500 py-3'>
+                    Even the all-powerfull Pointing has no control about the blind text it is an almost unorthographic life one day however a small line of blind text by the name lorem ipsum decided to leave for the far World of Grammar
+                </p>
+                <h1>author</h1>
             </div>
         </div>
     )
