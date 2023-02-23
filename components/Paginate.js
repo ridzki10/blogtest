4e2400fedbeaba562d30a64f72bcaa920ea29d80
ridzki10 <ridzki.okta96@gmail.com>
2023-02-23 19:49:42 +0700

    edit style in paginate component

--- components/Paginate.js
+++ components/Paginate.js
@@ -10,7 +10,7 @@ export default function Paginate({postPerPage, totalPost, paginate, onPreviousCl
             <nav aria-label="Page navigation example">
                 <ul className="flex list-style-none">
                 <li className="page-item disabled"><a
-                    className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-500 cursor-pointer focus:shadow-none"
+                    className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 cursor-pointer hover:text-gray-800 hover:bg-gray-200 focus:shadow-none focus:shadow-none"
                     onClick={onPreviousClick} aria-disabled="true">Previous</a></li>
                 {pageNumbers.map((num) => (
                     <li className="page-item" key={num}><a
@@ -18,7 +18,7 @@ export default function Paginate({postPerPage, totalPost, paginate, onPreviousCl
                     onClick={() => paginate(num)}>{num}</a></li>
                 ))}
                 <li className="page-item"><a
-                    className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+                    className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 cursor-pointer rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
                     onClick={onNextClick}>Next</a></li>
                 </ul>
             </nav>
